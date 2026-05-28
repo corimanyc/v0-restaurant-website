69340c7ee1941220d6238b8dd3439248ce2b69d9
v0agent <it+v0agent@vercel.com>
2026-05-28 03:28:36 +0000

    style: update Corima paragraph font weight to regular\n\nAdjust Corima paragraph to use '400' font weight.

--- app/page.tsx
+++ app/page.tsx
@@ -302,7 +302,7 @@ export default function Home() {
                 className="text-[24px] lg:text-[40px]"
                 style={{
                   fontFamily: "'Switzer', system-ui, sans-serif",
-                  fontWeight: 300,
+                  fontWeight: 400,
                   lineHeight: 1.2,
                   letterSpacing: '-0.02em',
                   color: '#CBCBCB',
